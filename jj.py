f8ddfb5f708edeaead8d8a4fa575fa63ec688f5c
Nyach <emmanuelombuna@gmail.com>
2020-01-13 20:57:06 +0300

    first commit

---
+++ jj.py
@@ -0,0 +1,11 @@
+new_var ='programming'
+sliced = new_var[3:7]
+slicedd = new_var[-8:-4]
+print(sliced)
+print(slicedd)
+
+str1 = 'mwai'
+str2 = 'kibaki'
+full_name = str1 + ' '+ str2
+print(full_name.upper())
+print(str1 +  str2)
